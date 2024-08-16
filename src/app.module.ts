b3d9a2fd9b7eba3fe601626ff7851fe21691b177
Sajjad Hoviegar <sajjad@Sajjads-MBP.homenet.telecomitalia.it>
2024-08-16 19:00:22 +0200

    Adding user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DatabaseModule, ProductsModule],
+  imports: [DatabaseModule, ProductsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createUserDto: Prisma.UserCreateInput) {
+    return this.databaseService.user.create({ data: createUserDto });
+  }
+
+  async findAll() {
+    return this.databaseService.user.findMany({});
+  }
+
+  async findOne(id: number) {
+    return this.databaseService.user.findUnique({
+      where: {
+        user_id: id,
+      },
+    });
+  }
+
+  async update(id: number, updateUserDto: Prisma.UserUpdateInput) {
+    return this.databaseService.user.update({
+      where: {
+        user_id: id,
+      },
+      data: updateUserDto,
+    });
+  }
+
+  async remove(id: number) {
+    return this.databaseService.user.delete({
+      where: {
+        user_id: id,
+      },
+    });
+  }
+}
